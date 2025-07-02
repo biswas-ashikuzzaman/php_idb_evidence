224f3bfe42835a9e82f18e9c40541a98ace3449d
hiclassic <biswasashikuzzaman@gmail.com>
2025-07-02 11:25:56 +0600

    Update factorial.php

--- factorial.php
+++ factorial.php
@@ -3,7 +3,7 @@
 <title>Factorial Program using loop in PHP</title>  
 </head>  
 <body>  
-<form method="post">  
+<!-- <form method="post">  
     Enter the Number:<br>  
     <input type="number" name="number" id="number">  
     <input type="submit" name="submit" value="Submit" />  
@@ -21,6 +21,38 @@
             }  
             echo $fact . "<br>";  
     }  
-?>  
+?>   -->
+ <!-- Loop ব্যবহার করে ফ্যাক্টোরিয়াল বের করা (ফর্মসহ)  -->
+<form method="post">
+    Enter a number: <input type="number" name="num">
+    <input type="submit" name="submit" value="Find Factorial">
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+    $n = $_POST['num'];
+    $fact = 1;
+
+    if ($n < 0) {
+        echo "Factorial of negative number is not defined.";
+    } elseif ($n == 0 || $n == 1) {
+        echo "Factorial of $n is 1";
+    } else {
+        for ($i = 2; $i <= $n; $i++) {
+            $fact *= $i;
+        }
+        echo "Factorial of $n is $fact";
+    }
+}
+?>
+<form action="" method="post">
+    <label for="num">Enter a number:</label>
+    <input type="number" name="num" id="num" required>
+    <input type="submit" name="submit" value="Calculate Factorial">
+</form>
+<?php 
+
+?>
+
 </body>  
 </html>  
