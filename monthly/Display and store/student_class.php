f9837697f9479782e32c206357ceb71ee93a7c9c
hiclassic <biswasashikuzzaman@gmail.com>
2025-07-10 11:28:37 +0600

    update

--- monthly/Display and store/student_class.php
+++ monthly/Display and store/student_class.php
@@ -1,49 +1,39 @@
-<?php session_start();
- ?>
 <?php
-require_once 'student_class.php';
+class Student {
+    public $id;
+    public $name;
+    public $batch;
 
-$message = '';
+    public function __construct($id, $name, $batch) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->batch = $batch;
+    }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    try {
-        $student = new Student(
-            $_POST['id'],
-            $_POST['name'],
-            $_POST['batch'],
-        );
-        $student->saveToFile("data.txt");
-        $message = "Student data saved successfully.";
-    } catch (Exception $e) {
-        $message = "Error: " . $e->getMessage();
+    public function saveToFile($filename) {
+        $data = "{$this->id},{$this->name},{$this->batch}\n";
+        file_put_contents($filename, $data, FILE_APPEND);
+    }
+
+    public static function displayStudents($filename) {
+        if (file_exists($filename)) {
+            echo "<h3 style='text-align:left; color:green; margin-left:20px;'>Registered Students:</h3>";
+            echo "<table border='1' style='margin-left:20px; font-size:18px; border-collapse: collapse;'>";
+            echo "<tr style='background-color:#f2f2f2;'><th>ID</th><th>Name</th><th>Batch</th></tr>";
+
+            $lines = file($filename);
+            foreach ($lines as $line) {
+                $line = trim($line);
+                if ($line) {
+                    list($id, $name, $batch) = explode(",", $line);
+                    echo "<tr><td>$id</td><td>$name</td><td>$batch</td></tr>";
+                }
+            }
+
+            echo "</table>";
+        } else {
+            echo "<p style='text-align:left; color:red; margin-left:20px;'>No student data found.</p>";
+        }
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Student Form</title>
-</head>
-<body>
-    <h1 style=" color:black; text-align:left; font-size:30px">Student Registration Form</h1>
-
-    <?php if ($message): ?>
-        <p><strong><?php echo $message; ?></strong></p>
-    <?php endif; ?>
-
-    <form style=" text-align:left; font-size:20px; color:Black; margin:20px; width:400px; padding:20px; border:1px solid #ccc; background-color:#f9f9f9;" method="POST" action="">
-        <label>ID:</label><br>
-        <input type="text" name="id" placeholder="Enter ID" required><br>
-
-        <label>Name:</label><br>
-        <input type="text" name="name" placeholder="Enter Name" required><br>
-
-        <label>Batch:</label><br>
-        <input type="text" name="batch" placeholder="Enter Batch" required><br>
-
-        <input type="submit" value="Submit">
-    </form>
-    <?php Student::displayStudents("data.txt"); ?>
-</body>
-</html>
